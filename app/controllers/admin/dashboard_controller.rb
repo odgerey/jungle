28866a67845f4543db658dfeab1e11bb53e9222b
Audrey <audreymedaino@gmail.com>
2020-07-24 00:06:31 -0400

    Dashboard controller has variables with the count

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['ADMIN_USER'], password: ENV['ADMIN_PASSWORD']
   def show
+    @products_total = Product.count(:all)
+    @categories_total = Category.count(:all)
   end
 end
